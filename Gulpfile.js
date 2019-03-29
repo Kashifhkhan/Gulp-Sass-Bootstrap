b51d6a365a2cb21246d01369c5b5f8e3020b5779
Kashif Khan <kasshh20@gmail.com>
2019-03-29 19:45:36 +0530

    gulp -  js compressor added

---
+++ Gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var sasslint = require("gulp-sass-lint");
+var classPrefix = require('gulp-class-prefix');
+var del = require('del');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+
+gulp.task('sass', function () {
+	return gulp.src('css/style.scss')
+	.pipe(sass({outputStyle: 'compressed'}, {errLogToConsole: true}))
+	.pipe(prefix("last 2 versions", "> 1%", "ie 8", "Android 2", "Firefox ESR"))
+	.pipe(gulp.dest('css/'))
+	.pipe(reload({stream:true}));
+});
+
+gulp.task('prefix', function() {
+	return gulp.src('node_modules/bootstrap/dist/css/bootstrap.css')
+	.pipe(classPrefix('me-'))
+	.pipe(gulp.dest('css/'));
+});
+
+gulp.task('sass-lint', function () {
+	return gulp.src('css/*.scss')
+		.pipe(sasslint({	
+			options: {
+				configFile: './.sass-lint.yml'
+            }			
+		}))
+		.pipe(sasslint.format())
+		.pipe(sasslint.failOnError())
+});
+
+gulp.task('browser-sync', function() {
+	browserSync.init(null, {
+        server: {
+            baseDir: "./"
+        }
+	});
+});
+
+gulp.task('clean', function() {
+  return del('js/dist/*');
+});
+
+var venderJs = [
+	'./node_modules/jquery-slim/dist/jquery.slim.js',
+	'./node_modules/bootstrap/dist/js/bootstrap.js'
+];
+
+gulp.task('minify:vendorjs', [
+	'clean'
+  ], function() {
+	return gulp.src(venderJs)
+	  .pipe(concat('main.js'))
+	  .pipe(gulp.dest('js/'));
+  });
+
+gulp.task('minifyjs',[
+	'clean',
+	'minify:vendorjs'
+  ], function() {
+	return gulp.src('js/*.js')
+	  .pipe(concat('main.min.js'))
+	  .pipe(uglify())
+	  .pipe(gulp.dest('js/dist'));
+  });
+
+gulp.task('default', ['sass', 'browser-sync'], function () {
+	gulp.watch("css/*.scss", ['sass']);
+	gulp.watch(["js/*.js", "./*.html"], reload);
+});
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Product order confirmation page
+
+Sample Gulp, Sass, Sass-lint, Browser-sync and Bootstrap project.
+## Installation
+
+### Requirements
+* Nodejs
+* npm
+* gulp
+
+`$ git clone https://github.com/Kashifhkhan/Gulp-Sass-Bootstrap`
+
+`$ cd Gulp-Sass-Bootstrap`
+
+`$ npm install`
+
+## Usage
+To run the project on local server trigger below command : 
+```
+$ gulp
+```
+It will serve the project on http://localhost:3000/
+
+
+## Development
+To run the sass lint trigger below command : 
+```
+$ gulp sass-lint
+```
